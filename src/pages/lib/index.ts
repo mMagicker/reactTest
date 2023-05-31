258373db1ae8be3bf014518eff0a160bbc8972ad
lazyggm <iixm@163.com>
2023-05-31 23:15:26 +0800

    feat(): merge

--- src/pages/lib/index.ts
+++ src/pages/lib/index.ts
@@ -1,7 +1,7 @@
-import Crypto from './Crypto'
-import HtmlToWord from './HtmlToWord'
-import SortHocPage from './SortHocPage'
-import Html2Canvas from "@/pages/lib/Html2Canvas";
+import Crypto from './crypto-js/Crypto'
+import HtmlToWord from './Html2/HtmlToWord'
+import SortHocPage from './react-sort-hoc/SortHocPage'
+import Html2Canvas from "./Html2/Html2Canvas";
 import G6 from "@/pages/lib/G6";
 
 export default {
