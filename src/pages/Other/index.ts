0766a01821ffe57e78237d3178683e5287568587
sorrymm <iixm@163.com>
2023-06-06 18:36:12 +0800

    rich text

---
+++ src/pages/Other/index.ts
@@ -0,0 +1,13 @@
+import Drag from "./Drag";
+import SelectedTable from "./SelectedTable";
+import SelectedTableAntd from "./SelectedTableAntd";
+import TableRow from "./TableRow";
+import LazyScroll from "./LazyScroll";
+
+export default {
+  Drag,
+  SelectedTable,
+  SelectedTableAntd,
+  TableRow,
+  LazyScroll,
+};
